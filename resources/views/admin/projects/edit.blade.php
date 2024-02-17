5467433768e88219ca2e9dc6ec1e93f4b62f76ae
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-02-17 12:21:18 +0100

    e

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -72,22 +72,30 @@
 
     <div class="mb-3">
         <h3>Ingredienti</h3>
-        @foreach($tags as $tag)
-            <div class="mb-3 form-check">
-                <input
-                    type="checkbox"
-                    class="form-check-input"
-                    id="tag{{ $tag->id }}"
-                    name="tags[]"
-                    value="{{ $tag->id }}"
-                    @if (in_array($tag->id, old('tags', $project->tags->pluck('id')->all()))) checked @endif
-                >
-                <label class="form-check-label" for="tag{{ $tag->id }}">{{ $tag->name }}</label>
-            </div>
-        @endforeach
-        <div class="invalid-feedback">
-            @error('tags') {{ $message }} @enderror
+        <div class="mytags">
+
+            @foreach($tags as $tag)
+
+                    <input
+                        type="checkbox"
+                        class="btn-check @error ('tags') is-invalid @enderror"
+                        id="tag{{ $tag->id }}"
+                        name="tags[]"
+                        value="{{ $tag->id }}"
+                        @if (in_array($tag->id, old('tags', $project->tags->pluck('id')->all()))) checked @endif
+          
+                    >
+                    <label class="btn btn-outline-dark" for="tag{{ $tag->id }}">{{ $tag->name }}</label>
+
+                @error('tags') 
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
+             
+            @endforeach
         </div>
+     
 
     </div>
 
