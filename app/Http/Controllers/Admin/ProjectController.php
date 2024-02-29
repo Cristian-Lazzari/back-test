4b60571218a2506a355276601d32d428911f4766
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-02-29 19:42:13 +0100

    asd

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -101,9 +101,9 @@ class ProjectController extends Controller
     }
 
 
-    public function update(Request $request, Project $project)
+    public function update(Request $request, $id)
     {
-       //$project = Project::where('id', $id)->firstOrFail();
+        $project = Project::where('id', $id)->firstOrFail();
 
         // validare i dati del form
         $request->validate($this->validations);
@@ -135,7 +135,7 @@ class ProjectController extends Controller
         $project->tags()->sync($data['tags'] ?? []);
 
         // ridirezionare su una rotta di tipo get
-        return to_route('admin.projects.show', ['project' => $project]);
+        return to_route('admin.projects.index', ['project' => $project]);
     }
 
 
