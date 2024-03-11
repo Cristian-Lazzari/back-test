90311d80bf70bbba552c57c5536a0e76e0742499
DavideFarci <yank92.df@gmail.com>
2024-03-11 12:34:23 +0100

    asdf

--- resources/views/admin/projects/showCategory.blade.php
+++ resources/views/admin/projects/showCategory.blade.php
@@ -120,6 +120,7 @@
                 <div class="fs-4 fw-bold">{{ $project->name }}</div>
                 <div class="fs-6 text-secondary">{{ $project->category->name }}</div>
                 <img class="my-image" src="https://db.dashboardristorante.it/public/images/or.png" alt="{{ $project->name }}">
+                {{-- <img class="my-image" src="{{ asset('public/storage/' . $project->image) }}" alt="{{ $project->name }}"> --}}
                 <div class="fs-6 text-primary fw-bold mb-2 pointer" data-bs-toggle="modal" data-bs-target="#exampleModal">Ingredienti</div>
                 
                 <!-- Modal -->
@@ -178,62 +179,7 @@
                         </button>
                     </form>
                 </div>
-            </div>
-
-
-
-        
-            {{-- @if($project->visible == 0)
-
-            <div class="myproj">
-                
-            @else 
-            
-            <div class="myproj-off">
-            
-            @endif
-
-                <section class="s1">
-                    <h4>{{$project->name}}</h4>
-                    <span class="cat">{{$project->category->name}}</span>
-                    <img class="my-image" src="{{ asset('public/storage/' . $project->image) }}" alt="{{ $project->title }}">
-                </section>
-                <section class="expire-mobile s2">
-                    <h5>Ingredienti:</h5>
-                    <p>
-                        @foreach ($project->tags as $tag)
-                            <span>{{ $tag->name }}</span>{{ !$loop->last ? ', ' : '.' }}
-                        @endforeach
-                    </p>
-                    <div class="price">€{{$project->price / 100}}</div>
-                </section>
-                <section class="s3">
-                    <a class="btn my-btn btn-warning w-100" href="{{ route('admin.projects.edit', $project->id) }}">Modifica</a>
-                        <form class="w-100" action="{{ route('admin.projects.destroy', ['project' =>$project])}}" method="post">
-                            @csrf
-                            @method('delete')
-                            <button class="btn btn-danger w-100" >Elimina</button>
-                        </form>
-
-                        @if($project->visible == 0)
-                            <form class="w-100" action="{{ route('admin.projects.updatestatus', $project->id)}}" method="post">
-                                @csrf
-
-                                <button class="btn btn-warning w-100" >Nascondi</button>
-                            </form>
-                        @else
-
-                        <form class="w-100" action="{{ route('admin.projects.updatestatus', $project->id)}}" method="post">
-                            @csrf
-
-                            <button class="btn btn-success w-100" >Mostra</button>
-                        </form>
-                        
-                        @endif
-                </section>
-            </div> --}}
-
-        
+            </div>        
         @endforeach
     </div>
 
