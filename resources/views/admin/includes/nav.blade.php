eca9aaecab329c4a484bbb9c25a68678a72b9956
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-03-15 10:45:28 +0100

    df

--- resources/views/admin/includes/nav.blade.php
+++ resources/views/admin/includes/nav.blade.php
@@ -3,14 +3,14 @@
 
         <div class="d-flex">
             <a class="btn btn-dark mybtdb" href="{{ route('admin.dashboard') }}">Dashboard</a>
-            <div class="notifications_container h-100 ms-2">
+            {{-- <div class="notifications_container h-100 ms-2">
                 <button class="btn btn-warning" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
                     <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-bell-fill" viewBox="0 0 16 16">
                         <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
                     </svg>
                 </button>
                 <div class="notifications_count">{{ count($notifications)}}</div>
-            </div>
+            </div> --}}
         </div>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
